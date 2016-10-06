0ba643f635968b0b49c52092ab8177c3350a525e
sumitpathak92 <pathaksumit92@gmail.com>
2016-10-06 15:00:02 +0530

    changes in the html report

--- codedocu/html_report1.py
+++ codedocu/html_report1.py
@@ -8,14 +8,15 @@ def main():
     with open('/home/sumit/personal files/codedocu/codedocu/doc2.html', 'w+') as f:
         f.write('<html>')
         f.write('<body>')
+
+        f.write("<b>ALPS CODE DOCUMENTATION RELEASE VERSION - 0.4.5</b>")
         f.write('<table border=1px;>')
         f.write('<tr><td style="background-color:red; text-align:center"> <b>FILE NAME</b> </td> <td style="text-align: center"><b>API NAME</b></td><td><b>COMMENT VALUE</b></td> </tr>')
 
         for k, v in first().iteritems():
             for key in v.keys():
-                f.write('<tr><td style=background-color:red><font style="background-color:red">%s</td><td>%s</td>:<td>%s</td>' %(k,key, v[key]))
+                f.write('<tr><td style=background-color:red><font style="background-color:red">%s</td><td>%s</td><td>%s</td>' %(k,key, v[key]))
 
-        f.write("<b>ALPS CODE DOCUMENTATION RELEASE VERSION - 0.4.5</b>")
         f.write('<br><br>')
         f.write('</tr>')
         f.write('</table>')
